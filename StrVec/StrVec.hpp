4aeee3b3f11ebe6ba388676b5a8c193e536a8a9f
tyf <1552068173@qq.com>
2021-09-19 10:54:19 +0800

    update

---
+++ StrVec/StrVec.hpp
@@ -0,0 +1,19 @@
+#ifndef _STRVEC_
+#define _STRVEC_
+
+#include <string>
+
+class StrVec
+{
+    public:
+
+
+    private:
+
+    std::string* elements;
+    std::string* first_free;
+    std::string* capacity;
+
+};
+
+#endif
