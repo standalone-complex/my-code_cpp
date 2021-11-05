a2ba97ad5ac0e46598300549fe597da6126d2f44
tyf <1552068173@qq.com>
2021-11-05 11:24:51 +0800

    update

---
+++ Leetcode/94. 二叉树的中序遍历_2.cc
@@ -0,0 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+void inorder(TreeNode* node, vector<int>& ret) {
+    if(!node) {
+        return;
+    }
+
+    inorder(node->left, ret);
+    ret.push_back(node->val);
+    inorder(node->right, ret);
+}
+
+class Solution {
+public:
+    vector<int> inorderTraversal(TreeNode* root) {
+        vector<int> ret;
+
+        inorder(root, ret);
+
+        return ret;
+    }
+};
+
