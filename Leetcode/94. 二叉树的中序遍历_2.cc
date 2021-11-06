dca073bd8c5a7de6313beac81f44e96a4c85a896
tyf <1552068173@qq.com>
2021-11-06 15:34:47 +0800

    迭代

--- Leetcode/94. 二叉树的中序遍历_2.cc
+++ Leetcode/94. 二叉树的中序遍历_2.cc
@@ -1,33 +1,37 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
+#include <stack>
+#include <vector>
 
-void inorder(TreeNode* node, vector<int>& ret) {
-    if(!node) {
-        return;
-    }
+using std::vector;
+using std::stack;
 
-    inorder(node->left, ret);
-    ret.push_back(node->val);
-    inorder(node->right, ret);
-}
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
 
+// 递归实际上是运行时栈，而迭代用一个显式的栈容器来解决，二者本质上是一致的
 class Solution {
 public:
     vector<int> inorderTraversal(TreeNode* root) {
         vector<int> ret;
+        stack<TreeNode*> simulator;
+        // 中序遍历
+        while(root!=NULL || !simulator.empty()) {
+            while(root != NULL) {
+            simulator.push(root);
+            root = root->left;
+            }
 
-        inorder(root, ret);
+            root = simulator.top();
+            simulator.pop();
+            ret.push_back(root->val);
+            root = root->right;
+        }
 
         return ret;
     }
 };
-
